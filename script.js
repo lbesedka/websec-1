d3c64e2963d5127023df52a00ec42101159fad88
Besedina Valeriia <75686525+lbesedka@users.noreply.github.com>
2022-09-04 13:55:42 +0400

    issues fixed

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-function whenButtonClicked(){
+function whenButtonClicked() {
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
-
-    value1 = parseFloat(value1);
-    value2 = parseFloat(value2);
 
     let result = 0;
     let result2 = "OK";
 
-    if (!isNaN(value1) && !isNaN(value2)){
-        switch (document.getElementById("select").value){
-            case "1": 
+    if (!isNaN(parseFloat(value1)) && isFinite(value1) && !isNaN(parseFloat(value2)) && isFinite(value2)) {
+        value1 = parseFloat(value1);
+        value2 = parseFloat(value2);
+        switch (document.getElementById("select").value) {
+            case "1":
                 result = value1 + value2;
                 break;
 
-            case "2": 
+            case "2":
                 result = value1 - value2;
                 break;
 
-            case "3": 
+            case "3":
                 result = value1 * value2;
                 break;
 
-            case "4": 
-                if(value2 != 0){
+            case "4":
+                if (Math.abs(value2) >= Number.EPSILON) {
                     result = value1 / value2;
                 }
-                else{
+                else {
                     result2 = "Ошибка. Деление на ноль";
                 }
                 break;
@@ -35,24 +34,19 @@ function whenButtonClicked(){
                 break;
         }
     }
-    else{
-            if (isNaN(value1) && isNaN(value2)){
-                result2 = "Ошибка. Оба значения не являются числом";
-            }
-            else if (isNaN(value1)){
-                result2 = "Ошибка. Первое значение не является числом";
-            }
-            else {
-                result2 = "Ошибка. Второе значения не являются числом";
-            }
-        }
-        if (result2 ==="OK"){
-            document.getElementById("results").value += result + "\n";
-        }
-        else{
-            document.getElementById("results").value += result2 + "\n";
-        }
 
+    else 
+    {
+        result2 = "Ошибка. Введены некорректные данные."
     }
 
- 
+    if (result2 === "OK") {
+        document.getElementById("results").value += result + "\n";
+    }
+    else {
+        document.getElementById("results").value += result2 + "\n";
+    }
+
+}
+
+
